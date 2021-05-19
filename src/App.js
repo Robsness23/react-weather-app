e0c935b882b987c5ce2dcdc1b223854507511815
Robyn <78262868+Robsness23@users.noreply.github.com>
2021-05-19 20:59:43 +0100

    Added GitHub repo URL & LinkedIn URL

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">Weather App</div>
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by <a href="https://www.linkedin.com/in/robyn-smith-27187910b/" target="_blank">Robyn Smith</a> and is {" "}
+      <a href="https://github.com/Robsness23/react-weather-app" target="_blank">
+        open-sourced on GitHub
+      </a>
+      </footer>
+      </div>
   );
 }
 
-export default App;
