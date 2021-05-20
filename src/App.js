7ed45ce349d1d6e10cebf961cfa8eeb8add35db5
Robyn <78262868+Robsness23@users.noreply.github.com>
2021-05-20 19:57:18 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+        <h1>Weather App</h1>
       <footer>
         This project was coded by <a href="https://www.linkedin.com/in/robyn-smith-27187910b/" target="_blank">Robyn Smith</a> and is {" "}
-      <a href="https://github.com/Robsness23/react-weather-app" target="_blank">
-        open-sourced on GitHub
-      </a>
+        <a href="https://github.com/Robsness23/react-weather-app" target="_blank">
+          open-sourced on GitHub
+        </a>
       </footer>
       </div>
+    </div>
   );
 }
 
