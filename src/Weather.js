60b3d5bdb73e7935b91e7dcbcb6842d0c5058978
Robyn <78262868+Robsness23@users.noreply.github.com>
2021-05-20 22:14:55 +0100

    Added basic CSS

--- src/Weather.js
+++ src/Weather.js
@@ -7,10 +7,10 @@ export default function Weather() {
             <form>
                 <div className="row">
                     <div className="col-9">
-                <input type="search" placeholder="Enter a city..." className="form-control" />
+                <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on" />
                     </div>
                     <div className="col-3">
-                <input type="submit" value="Search" className="btn btn-primary" />
+                <input type="submit" value="Search" className="btn btn-primary w-100" />
                     </div>
                 </div>
             </form>
@@ -19,19 +19,24 @@ export default function Weather() {
             <li>Thursday 20:00</li>
             <li>Partly cloudy</li>
         </ul>
-        <div className="row">
-            <div className="col-6">
-                <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Partly Cloudy" />
-                6°C
+            <div className="row mt-3">
+                <div className="col-6">
+                    <div className="clearfix">
+                        <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Partly Cloudy" className="float-left" />
+                        <div className="float-left">
+                            <span className="temperature">6</span>
+                            <span className="unit">°C</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-6">
+                    <ul>
+                        <li>Precipitation: 80%</li>
+                        <li>Humidity: 73%</li>
+                        <li>Wind: 15km/h</li>
+                    </ul>
+                </div>
             </div>
-            <div className="col-6">
-                <ul>
-                    <li>Precipitation: 80%</li>
-                    <li>Humidity: 73%</li>
-                    <li>Wind: 15km/h</li>
-                </ul>
-            </div>
-        </div>
         </div>
     )
 }
