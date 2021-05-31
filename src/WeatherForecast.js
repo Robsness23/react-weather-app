6dd72d62fb9d7b1cfbe210a9646b1ff0a053c8ad
Robyn <78262868+Robsness23@users.noreply.github.com>
2021-05-31 10:20:16 +0100

    Refactored code and fixed warnings

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,6 +16,17 @@ export default function WeatherForecast(props) {
     setLoaded(true);
     }
 
+    function load() {
+    let apiKey = "1d9fb2d6f9899977e52e0b60c9d8f79b";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+    }
+
     if (loaded) {
         return (
             <div className="WeatherForecast">
@@ -27,6 +38,8 @@ export default function WeatherForecast(props) {
                                 <WeatherForecastDay data={dailyForecast} />
                                 </div>
                                 )
+                            } else {
+                                return null;
                             }
                         })}
                     
@@ -34,12 +47,7 @@ export default function WeatherForecast(props) {
             </div>
         )
     } else {
-    let apiKey = "1d9fb2d6f9899977e52e0b60c9d8f79b";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
-
-    axios.get(apiUrl).then(handleResponse);
+    load();
 
     return null;
     }
