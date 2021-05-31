dd6fc941560f9bd8ff9144c329c56ab7e43b2d7b
Robyn <78262868+Robsness23@users.noreply.github.com>
2021-05-31 08:33:32 +0100

    Integrated API call into WeatherForecast component with long & lat coords

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,19 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+    function handleResponse(response) {
+        console.log(response.data);
+    }
+
+    let apiKey = "1d9fb2d6f9899977e52e0b60c9d8f79b";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=mettric`
+
+    axios.get(apiUrl).then(handleResponse);
     return (
         <div className="WeatherForecast">
             <div className="row">
